b1de51afca6b2c244f903a5d779b672302efab15
DrminaEmad <drmina200015@gmail.com>
2025-08-29 19:14:02 +0000

    4th commit

--- svelte_tut/src/routes/+page.svelte
+++ svelte_tut/src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h2><Hello world /h2>
+<h2>Hello world </h2>
