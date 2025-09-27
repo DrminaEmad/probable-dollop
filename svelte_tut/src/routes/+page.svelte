8c2034191129d41c62465398ca373cb6fccbaf54
DrminaEmad <drmina200015@gmail.com>
2025-09-27 20:53:07 +0000

    todos

--- svelte_tut/src/routes/+page.svelte
+++ svelte_tut/src/routes/+page.svelte
@@ -1,8 +1,74 @@
-<!-- <nav>
+<!-- <nav
 	<a href="/">home</a>
 	<a href="/about">about</a>
 	<a href="/blog">blog</a>
 </nav> -->
+<script>
+	let {data} = $props()
+</script>
 
 <h1>home</h1>
 <p>this is the home page.</p>
+
+
+<div class="centered">
+	<h1>todos</h1>
+
+<form action="POST">
+	<label>add a todo :
+		<input 
+		type="text"
+		name="description"
+		autocomplete="off"/>
+	</label>
+</form>
+
+<ul class="todos">
+			{#each data.todos as todo(todo.id) }
+			<li>
+				{todo.description}
+			</li>			
+
+			{/each}
+
+</ul>
+</div>
+
+
+<style>
+	.centered {
+		max-width: 20em;
+		margin: 0 auto;
+	}
+
+	label {
+		width: 100%;
+	}
+
+	input {
+		flex: 1;
+	}
+
+	span {
+		flex: 1;
+	}
+
+	button {
+		border: none;
+		background: url(./remove.svg) no-repeat 50% 50%;
+		background-size: 1rem 1rem;
+		cursor: pointer;
+		height: 100%;
+		aspect-ratio: 1;
+		opacity: 0.5;
+		transition: opacity 0.2s;
+	}
+
+	button:hover {
+		opacity: 1;
+	}
+
+	.saving {
+		opacity: 0.5;
+	}
+</style>
